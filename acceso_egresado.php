77aaf455ddb5115aaf3fea10facc1dcd5f1f50b6
ignacio <danis.manchu@itsae.edu.ec>
2019-10-15 15:48:03 -0500

    Se ha modificado la vista de acceso de egresado

--- acceso_egresado.php
+++ acceso_egresado.php
@@ -2,26 +2,26 @@
 <html lang="es">
   <head>
 
-    <meta charset="utf-8">    
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
     <title>SEI | Sistema Egresado Itsae</title>
 
     <link rel="icon" href="assets/img/itsae.png" type="image/ico" />
-    <link href="assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">   
-    <link href="assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">   
-    <link href="assets/vendors/nprogress/nprogress.css" rel="stylesheet">    
-    <link href="assets/vendors/animate.css/animate.min.css" rel="stylesheet">   
+    <link href="assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="assets/vendors/animate.css/animate.min.css" rel="stylesheet">
     <link href="assets/build/css/custom.min.css" rel="stylesheet">
-    
+
   </head>
 
   <body class="login">
     <div>
-      <a class="hiddenanchor" id="signup"></a>     
+      <a class="hiddenanchor" id="signup"></a>
 
       <div class="login_wrapper">
-        <div class="animate form login_form"> 
+        <div class="animate form login_form">
           <section class="login_content">
 
 
@@ -30,28 +30,32 @@
               <h1><i class="fa fa-user"></i>SEI</h1>
               <h2>Iniciar sesión</h2>
                 <div>
-                  <input type="text" class="form-control" placeholder="Usuario" required="" name="dni" />
-                </div>               
+                  <input type="text" class="form-control" placeholder="Identidad" required="" name="dni" />
+                </div>
+
+                 <div>
+                  <input type="text" class="form-control" placeholder="Password" required="" name="dni" />
+                </div>
                 <div>
-                  <input class="btn btn-default submit" type="submit" value="Acceder" name="entrar">               
+                  <input class="btn btn-default submit" type="submit" value="Acceder" name="entrar">
                 </div>
 
               <div class="clearfix"></div>
 
-              <div class="separator">                
+              <div class="separator">
 
                 <div class="clearfix"></div>
                 <br />
 
-                <div>                  
+                <div>
                   <p>©2019 Desarrollado por 5to nivel de sistemas</p>
                 </div>
               </div>
             </form>
-            
+
 
           </section>
-        </div>       
+        </div>
       </div>
     </div>
   </body>
