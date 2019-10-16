e18d3b5dc0a5d5031dd3a214694d97c4eaf4d362
ignacio <danis.manchu@itsae.edu.ec>
2019-10-16 01:41:14 -0500

    Se ha aumentado 2 imputs en el archivo egresado.php y de la misma para registrar los datos a la base de Datos, en el archivo registrar_egresado.ph

--- admin/clases/insertar_egresado.php
+++ admin/clases/insertar_egresado.php
@@ -1,32 +1,36 @@
-<?php 
+<?php
 
 //Compobando si los datos vienen cargados desde el formulario.
- if (isset($_POST['txt_dniper']) && isset($_POST['txt_nombreper']) && isset($_POST['txt_apellidoper']) && isset($_POST['carrera'])&& isset($_POST['mencion']) && isset($_POST['modalidad']) && isset($_POST['jornada']) && isset($_POST['nacionalidad'])) {   	
+ if (isset($_POST['txt_dniper']) && isset($_POST['txt_nombreper']) && isset($_POST['txt_apellidoper']) && isset($_POST['txt_emailper']) && isset($_POST['txt_passwordper']) && isset($_POST['carrera'])&& isset($_POST['mencion']) && isset($_POST['modalidad']) && isset($_POST['jornada']) && isset($_POST['nacionalidad'])) {
 
 
-         include_once('conexion.php');     
-   	      
+         include_once('conexion.php');
+
    	      $dni_persona      = $_POST['txt_dniper'];
    	      $nombre_persona   = $_POST['txt_nombreper'];
           $apellido_persona = $_POST['txt_apellidoper'];
+          $email_persona    = $_POST['txt_emailper'];
+          $password_persona = $_POST['txt_passwordper'];
           $carrera          = $_POST['carrera'];
           $mencion          = $_POST['mencion'];
           $modalidad        = $_POST['modalidad'];
           $jornada          = $_POST['jornada'];
           $nacionalidad     = $_POST['nacionalidad'];
-   	      
 
-   	      $insertar=$pdo->prepare("INSERT INTO persona(dni_persona, nombres_persona, apellidos_persona, carrera, mencion,modalidad,jornada, pais) VALUES(?,?,?,?,?,?,?,?)");
+
+   	      $insertar=$pdo->prepare("INSERT INTO persona(dni_persona, nombres_persona, apellidos_persona, email_persona, password_persona, carrera, mencion,modalidad,jornada, pais) VALUES(?,?,?,?,?,?,?,?,?,?)");
 
    	          $insertar->bindParam(1, $dni_persona);
    	          $insertar->bindParam(2, $nombre_persona);
               $insertar->bindParam(3, $apellido_persona);
-              $insertar->bindParam(4, $carrera); 
-              $insertar->bindParam(5, $mencion);
-              $insertar->bindParam(6, $modalidad);
-              $insertar->bindParam(7, $jornada);
-              $insertar->bindParam(8, $nacionalidad);  	        
-   	         
+              $insertar->bindParam(4, $email_persona);
+              $insertar->bindParam(5, $password_persona);
+              $insertar->bindParam(6, $carrera);
+              $insertar->bindParam(7, $mencion);
+              $insertar->bindParam(8, $modalidad);
+              $insertar->bindParam(9, $jornada);
+              $insertar->bindParam(10, $nacionalidad);
+
 
    	          if ($insertar->execute()) {
    	          	    echo "Datos almacenados correctamente";
@@ -36,7 +40,7 @@
 
    	          $insertar->execute();
    	          header('location:../egresado.php');
-   	          
+
 
    } else {
    	 echo "Falta algunos datos por llenar";
--- admin/egresado.php
+++ admin/egresado.php
@@ -136,6 +136,22 @@
                                   </div>
                                 </div>
 
+                                 <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Email *<span class="required"></span>
+                                  </label>
+                                  <div class="col-md-9 col-sm-9 col-xs-12">
+                                    <input type="text" id="last-name" name="txt_emailper" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                                </div>
+
+                                 <div class="form-group">
+                                  <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Password *<span class="required"></span>
+                                  </label>
+                                  <div class="col-md-9 col-sm-9 col-xs-12">
+                                    <input type="password" id="last-name" name="txt_passwordper" required="required" class="form-control col-md-7 col-xs-12">
+                                  </div>
+                                </div>
+
                                    <?php
                                       require_once('clases/conexion.php');
 
